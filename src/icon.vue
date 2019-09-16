45149f6923c12343e8e500d12f37c3764195980e
suyuan <suyuan326@qq.com>
2019-09-16 10:17:06 +0800

    展示组件Button

--- src/icon.vue
+++ src/icon.vue
@@ -1,16 +1,15 @@
 <template>
     <svg class="icon">
-        <use :xlink:href=`#i-${name}`></use>
+        <use :xlink:href="`#i-${name}`"></use>
     </svg>
 </template>
 <script>
     import "./svg";
     export default {
         props: ['name']
-      //add
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
     .icon {
         width: 1em;
         height: 1em;
