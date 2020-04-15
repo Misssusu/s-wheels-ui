de401e9588f0f0c67d30690b775c8238fdacadec
suyuan <suyuan326@qq.com>
2020-04-15 16:54:48 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import Button from "./src/button";
-import ButtonGroup from "./src/buttonGroup";
-import Icon from "./src/icon";
-import Collapse from "./src/collapse";
-import CollapseItem from "./src/collapse-item";
-import Input from "./src/input";
-import Popover from "./src/popover";
-import Tabs from "./src/tabs";
-import TabsNav from "./src/tabs-nav";
-import TabsBody from "./src/tabs-body";
-import TabsItem from "./src/tabs-item";
-import TabsPane from "./src/tabs-pane";
+import Button from "./src/button/button";
+import ButtonGroup from "./src/button/buttonGroup";
+import Icon from "./src/icon/icon";
+import Collapse from "./src/collapse/collapse";
+import CollapseItem from "./src/collapse/collapse-item";
+import Input from "./src/input/input";
+import Popover from "./src/popover/popover";
+import Tabs from "./src/tabs/tabs";
+import TabsNav from "./src/tabs/tabs-nav";
+import TabsBody from "./src/tabs/tabs-body";
+import TabsItem from "./src/tabs/tabs-item";
+import TabsPane from "./src/tabs/tabs-pane";
 
 
 
